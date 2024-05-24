14cf8df1c0918ebacb2362f2244dc86107c81a2a
kacpe <kacper.siakowski47@gmail.com>
2024-05-24 13:03:42 +0200

    changes in accounts app

---
+++ accounts/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+# from accounts.models import UserProfile
+
+
+# class UserProfileModelForm(forms.ModelForm):
+#     class Meta:
+#         model = UserProfile
+#         fields = ["first_name", "last_name", "email", "role"]
