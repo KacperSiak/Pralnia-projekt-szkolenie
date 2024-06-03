4b75241bec31d597e4bef470f01a1fc7d1197159
Alicja <alaadamczuk1@gmail.com>
2024-06-03 18:22:30 +0200

    cars

--- cars/models.py
+++ cars/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Cars(models.Model):
     brand = models.CharField(max_length=255)
     model = models.CharField(max_length=100)
-    registration_number = models.CharField(max_length=7)
-    technical_review_date = models.DateField(max_length=9)
+    registration_number = models.CharField(max_length=8)
+    technical_review_date = models.DateField(max_length=10)
     insurance_date = models.DateField(max_length=10)
 
     def __str__(self):
